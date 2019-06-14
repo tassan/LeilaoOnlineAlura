f19149a86705bd99df5673acb3e575cda355e2f8
Flávio Tassan <tassanf@gmail.com>
2019-06-14 09:09:46 -0300

    Testes importados do console para xunit

--- Alura.LeilaoOnline.Tests/LeilaoTests.cs
+++ Alura.LeilaoOnline.Tests/LeilaoTests.cs
@@ -1,10 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Alura.LeilaoOnline.Core;
+using Xunit;
 
 namespace Alura.LeilaoOnline.Tests
 {
     public class LeilaoTests
     {
+        [Fact]
+        public void LeilaoComVariosLances()
+        {
+            /// Padrão ArrangeActAssert
+            /// http://wiki.c2.com/?ArrangeActAssert
+            /// 
+
+            // Arranje - Configura Cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(maria, 990);
+
+            //Act - Método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
+        public void LeilaoComApenasUmLance()
+        {
+            // Arranje - Configura Cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+
+            //Act - Método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
     }
 }
