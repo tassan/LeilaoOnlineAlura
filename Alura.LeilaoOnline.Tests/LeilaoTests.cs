d4b4ccb6f5e4c857d34fc5de80d3dbed2c148a97
Flávio Tassan <tassanf@gmail.com>
2019-06-14 09:21:38 -0300

    Testes de Lances Ordenados por valor e Leilao com tres clientes

--- Alura.LeilaoOnline.Tests/LeilaoTests.cs
+++ Alura.LeilaoOnline.Tests/LeilaoTests.cs
@@ -6,6 +6,55 @@ namespace Alura.LeilaoOnline.Tests
     public class LeilaoTests
     {
         [Fact]
+        public void LeilaoComTresClientes()
+        {
+            // Arranje - Configura Cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+            var beltrano = new Interessada("Beltrano", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(maria, 990);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(beltrano, 1400);
+
+            //Act - Método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 1400;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+            Assert.Equal(beltrano, leilao.Ganhador.Cliente);
+        }
+
+        [Fact]
+        public void LeilaoComLancesOrdenadosPorValor()
+        {
+            // Arranje - Configura Cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(maria, 990);
+            leilao.RecebeLance(fulano, 1000);
+
+            //Act - Método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
         public void LeilaoComVariosLances()
         {
             /// Padrão ArrangeActAssert
