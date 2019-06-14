d911b1b83ae459cb3b088ab69a21fd0c30c7cb2d
Flávio Tassan <tassanf@gmail.com>
2019-06-14 11:00:44 -0300

    Testes refatorados para explicitarem os cenários de teste

---
+++ Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
@@ -0,0 +1,49 @@
+﻿using Alura.LeilaoOnline.Core;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoTerminaPregao
+    {
+        // Padrão Arrange-Act-Assert
+        // http://wiki.c2.com/?ArrangeActAssert
+
+        [Theory]
+        [InlineData(1000, new double[] { 800, 900, 1000, 990 })]
+        [InlineData(1000, new double[] { 800, 900, 990, 1000 })]
+        [InlineData(800, new double[] { 800 })]
+        public void RetornaMaiorValorDadoLeilaoComPeloMenosUmLance(double valorEsperado, double[] ofertas)
+        {
+            // Arranje - Configura Cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            foreach (var valor in ofertas)
+                leilao.RecebeLance(fulano, valor);
+
+            //Act - Método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
+        public void RetornaZeroDadoLeilaoSemLances()
+        {
+            // Arranje - Configura Cenário
+            var leilao = new Leilao("Van Gogh");
+
+            // Act - Método sob teste
+            leilao.TerminaPregao();
+
+            // Assert
+            var valorEsperado = 0;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+    }
+}
