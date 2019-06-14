c397cb13ca7c89dee20c91cb05aceb1ad5de4552
Flávio Tassan <tassanf@gmail.com>
2019-06-14 11:17:10 -0300

    Adicionado 'Estado' do Leilão

--- Alura.LeilaoOnline.Core/Leilao.cs
+++ Alura.LeilaoOnline.Core/Leilao.cs
@@ -3,22 +3,38 @@ using System.Linq;
 
 namespace Alura.LeilaoOnline.Core
 {
+    public enum EstadoLeilao
+    {
+        LeilaoEmAndamento,
+        LeilaoFinalizado
+    }
+
     public class Leilao
     {
         private readonly IList<Lance> _lances;
         public IEnumerable<Lance> Lances => _lances;
         public string Peca { get; set; }
         public Lance Ganhador { get; private set; }
+        public EstadoLeilao Estado { get; private set; }
 
         public Leilao(string peca)
         {
             Peca = peca;
             _lances = new List<Lance>();
+            Estado = EstadoLeilao.LeilaoEmAndamento;
         }
 
-        public void RecebeLance(Interessada cliente, double valor) => _lances.Add(new Lance(cliente, valor));
+        public void RecebeLance(Interessada cliente, double valor)
+        {
+            if (Estado == EstadoLeilao.LeilaoEmAndamento)
+                _lances.Add(new Lance(cliente, valor));
+        }
 
         public void IniciaPregao() { }
-        public void TerminaPregao() => Ganhador = Lances.DefaultIfEmpty(new Lance(null, 0)).OrderBy(l => l.Valor).LastOrDefault();
+        public void TerminaPregao()
+        {
+            Ganhador = Lances.DefaultIfEmpty(new Lance(null, 0)).OrderBy(l => l.Valor).LastOrDefault();
+            Estado = EstadoLeilao.LeilaoFinalizado;
+        }
     }
 }
