fdbf001df667ddbdccbf2bcd34551496b263ed33
Flávio Tassan <tassanf@gmail.com>
2019-06-14 11:17:43 -0300

    Testes para cenário 3

--- Alura.LeilaoOnline.Tests/LeilaoRecebeOferta.cs
+++ Alura.LeilaoOnline.Tests/LeilaoRecebeOferta.cs
@@ -1,17 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xunit;
+﻿using Xunit;
 using Alura.LeilaoOnline.Core;
+using System.Linq;
 
 namespace Alura.LeilaoOnline.Tests
 {
     public class LeilaoRecebeOferta
     {
-        [Fact]
-        public void NaoPermiteNovosLancesDadoLeilaoFinalizado()
+        [Theory]
+        [InlineData(4, new double[] { 1000, 1200, 1400, 1300 })]
+        [InlineData(2, new double[] { 800, 900 })]
+        public void NaoPermiteNovosLancesDadoLeilaoFinalizado(int qtdeEsperada, double[] ofertas)
         {
+            // Arranje - Configura Cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
 
+            foreach (var valor in ofertas)
+                leilao.RecebeLance(fulano, valor);
+
+            leilao.TerminaPregao();
+
+            // Act - Método sob teste
+            leilao.RecebeLance(fulano, 1000);
+
+            // Assert
+            var qtdeObtido = leilao.Lances.Count();
+
+            Assert.Equal(qtdeEsperada, qtdeObtido);
         }
     }
 }
