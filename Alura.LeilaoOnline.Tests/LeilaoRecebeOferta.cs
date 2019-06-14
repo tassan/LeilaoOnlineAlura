f72fceff4e424ebd2aa3569f19c5d76affad8ec3
Flávio Tassan <tassanf@gmail.com>
2019-06-14 11:04:45 -0300

    Validação para cenário de testes 3

---
+++ Alura.LeilaoOnline.Tests/LeilaoRecebeOferta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Alura.LeilaoOnline.Core;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoRecebeOferta
+    {
+        [Fact]
+        public void NaoPermiteNovosLancesDadoLeilaoFinalizado()
+        {
+
+        }
+    }
+}
