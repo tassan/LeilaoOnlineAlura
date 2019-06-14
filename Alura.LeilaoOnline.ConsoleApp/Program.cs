1c2f6a6cfb5488ea836fd5499ad43bcace35802b
Flávio Tassan <tassanf@gmail.com>
2019-06-14 08:40:11 -0300

    Definido padrão Arrange-Act-Assert

--- Alura.LeilaoOnline.ConsoleApp/Program.cs
+++ Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -7,6 +7,11 @@ namespace Alura.LeilaoOnline.ConsoleApp
     {
         static void Main()
         {
+            /// Padrão ArrangeActAssert
+            /// http://wiki.c2.com/?ArrangeActAssert
+            /// 
+
+            // Arranje - Configura Cenário
             var leilao = new Leilao("Van Gogh");
             var fulano = new Interessada("Fulano", leilao);
             var maria = new Interessada("Maria", leilao);
@@ -16,9 +21,17 @@ namespace Alura.LeilaoOnline.ConsoleApp
             leilao.RecebeLance(fulano, 1000);
             leilao.RecebeLance(maria, 990);
 
+            //Act - Método sob teste
             leilao.TerminaPregao();
 
-            Console.WriteLine(leilao.Ganhador.Valor);
+            //Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            if (valorEsperado == valorObtido)
+                Console.WriteLine("Teste OK");
+            else
+                Console.WriteLine($"Teste FALHOU!\nEsperado: {valorEsperado}\nObtido: {valorObtido}");
         }
     }
 }
